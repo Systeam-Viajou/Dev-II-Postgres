cd62ea724df93f5948476dbe874438906db27c57
evellynNakamura <evellyn.nakamura@germinare.org.br>
2024-10-08 18:00:15 -0300

    removendo classe

--- src/main/java/com/example/viajouapi/repositorys/ClassificacaoRpository.java
+++ src/main/java/com/example/viajouapi/repositorys/ClassificacaoRpository.java
@@ -4,4 +4,5 @@ import com.example.viajouapi.models.Classificacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClassificacaoRpository extends JpaRepository<Classificacao, Long> {
+
 }
