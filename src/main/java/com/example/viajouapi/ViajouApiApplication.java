29fd8eb539abef0369b5ab627189ffdf8e0b5cef
Jampeta <joao.pedro@germinare.org.br>
2024-10-16 10:41:57 -0300

    terminando

--- src/main/java/com/example/viajouapi/ViajouApiApplication.java
+++ src/main/java/com/example/viajouapi/ViajouApiApplication.java
@@ -2,11 +2,20 @@ package com.example.viajouapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ViajouApiApplication {
 
     public static void main(String[] args) {
+        // Carregar o arquivo .env
+        Dotenv dotenv = Dotenv.configure().load();
+
+        // Definir variáveis de ambiente programaticamente
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
         SpringApplication.run(ViajouApiApplication.class, args);
     }
 
